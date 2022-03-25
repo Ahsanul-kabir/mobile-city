aafbe69e02e4ebeaad96e7942aaafab621a600e6
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-03-25 23:56:17 +0600

    fake data and basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import fakeData from './fakeData/fakeData.json';
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [mobiles, setMobiles] = useState([]);
+  useEffect(() => {
+    setMobiles(fakeData);
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>{mobiles.length}</h1>
     </div>
   );
 }
